97064bec508bb457faa7589f5ded29a356f13099
XiaoyueZhanginNCL <zhangxiaoyue2022@gmail.com>
2024-02-02 12:05:29 +0000

    props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((v) => {
+        return <Note key={v.key} title={v.title} content={v.content} />;
+      })}
       <Footer />
     </div>
   );
 }
 
 export default App;
+
